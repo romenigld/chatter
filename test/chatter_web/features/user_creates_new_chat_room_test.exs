f64089610307e04cc2219a08ed64e1214f145a3c
Romenig Lima Damasio <romenigld@gmail.com>
2021-11-17 14:04:41 +0100

    Refactoring

--- test/chatter_web/features/user_creates_new_chat_room_test.exs
+++ test/chatter_web/features/user_creates_new_chat_room_test.exs
@@ -3,10 +3,28 @@ defmodule ChatterWeb.UserCreatesNewChatRoomTest do
 
   test "user creates a new chat room successfully", %{session: session} do
     session
-    |> visit(Routes.chat_room_path(@endpoint, :index))
-    |> click(Query.link("New chat room"))
-    |> fill_in(Query.text_field("Name"), with: "elixir")
+    # |> visit(Routes.chat_room_path(@endpoint, :index))
+    |> visit(rooms_index())
+    # |> click(Query.link("New chat room"))
+    |> click(new_chat_link())
+    # |> fill_in(Query.text_field("Name"), with: "elixir")
+    # |> click(Query.button("Submit"))
+    |> create_chat_room(name: "elixir")
+    # |> assert_has(Query.data("role", "room-title", text: "elixir"))
+    |> assert_has(room_title("elixir"))
+  end
+
+  defp rooms_index, do: Routes.chat_room_path(@endpoint, :index)
+
+  defp new_chat_link, do: Query.link("New chat room")
+
+  defp create_chat_room(session, name: name) do
+    session
+    |> fill_in(Query.text_field("Name"), with: name)
     |> click(Query.button("Submit"))
-    |> assert_has(Query.data("role", "room-title", text: "elixir"))
+  end
+
+  defp room_title(title) do
+    Query.data("role", "room-title", text: title)
   end
 end
